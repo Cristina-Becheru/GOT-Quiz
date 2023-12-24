8555ef37934c1467df043f4db396e1cc6a921286
Cristina-Becheru <>
2023-12-24 20:37:35 +0000

    Add event listener to handle user clicks on answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,58 +5,58 @@ document.addEventListener("DOMContentLoaded", function () {
     let optionFinal = 0;
 
     const allQuestions = [
-    {
-        question: "How many swords make up the Iron Throne?",
-        choices: ["500", "1000", "2000", "5000"],
-        answer: 2
-    },
+        {
+            question: "How many swords make up the Iron Throne?",
+            choices: ["500", "1000", "2000", "5000"],
+            answer: 2
+        },
 
-    {
-        question: "What does Valar Morghulis mean?",
-        choices: ["All men must die", "What is dead may never die", "Never say never", "All men must first live"],
-        answer: 1
-    },
-    {
-        question: "What House rules the Kingdom of the North?",
-        choices: ["Tully", "Tyrell", "Stark", "Martell"],
-        answer: 3
-    }
-    , {
-        question: "What is Jon Snow's real name?",
-        choices: ["Jon", "Aerys", "Aegon", "Rhaegar"],
-        answer: 3
-    }
-    , {
-        question: "On which continent are the Seven Kingdoms located?",
-        choices: ["Essos", "Westeros", "Hyboria", "Valyria"],
-        answer: 2
-    }
-    , {
-        question: "What is the name of Arya's direwolf?",
-        choices: ["Nymeria", "Lady", "Ghost", "Grey Wind"],
-        answer: 1
-    }
-    , {
-        question: "Who betrays Robb Stark at the Red Wedding?",
-        choices: ["Randyll Tarlly", "Balon Greyjoy", "Walder Frey", "Hoster Tully"],
-        answer: 3
-    }
-    , {
-        question: "How many seasons of the Game of Thrones series are there?",
-        choices: ["Four", "Five", "Seven", "Eight"],
-        answer: 4
-    }
-    , {
-        question: "HBO's Game of Thrones is adapted from what series of novels?",
-        choices: ["A song of ice and fire", "Earthsea", "The broken empire", "The kingkiller chronicle"],
-        answer: 1
-    }
-    , {
-        question: "What is the name of the last surviving dragon in Game of Thrones?",
-        choices: ["Drogon", "Rhaegal", "Viserion", "Tatsu"],
-        answer: 1
-    }
-];
+        {
+            question: "What does Valar Morghulis mean?",
+            choices: ["All men must die", "What is dead may never die", "Never say never", "All men must first live"],
+            answer: 1
+        },
+        {
+            question: "What House rules the Kingdom of the North?",
+            choices: ["Tully", "Tyrell", "Stark", "Martell"],
+            answer: 3
+        }
+        , {
+            question: "What is Jon Snow's real name?",
+            choices: ["Jon", "Aerys", "Aegon", "Rhaegar"],
+            answer: 3
+        }
+        , {
+            question: "On which continent are the Seven Kingdoms located?",
+            choices: ["Essos", "Westeros", "Hyboria", "Valyria"],
+            answer: 2
+        }
+        , {
+            question: "What is the name of Arya's direwolf?",
+            choices: ["Nymeria", "Lady", "Ghost", "Grey Wind"],
+            answer: 1
+        }
+        , {
+            question: "Who betrays Robb Stark at the Red Wedding?",
+            choices: ["Randyll Tarlly", "Balon Greyjoy", "Walder Frey", "Hoster Tully"],
+            answer: 3
+        }
+        , {
+            question: "How many seasons of the Game of Thrones series are there?",
+            choices: ["Four", "Five", "Seven", "Eight"],
+            answer: 4
+        }
+        , {
+            question: "HBO's Game of Thrones is adapted from what series of novels?",
+            choices: ["A song of ice and fire", "Earthsea", "The broken empire", "The kingkiller chronicle"],
+            answer: 1
+        }
+        , {
+            question: "What is the name of the last surviving dragon in Game of Thrones?",
+            choices: ["Drogon", "Rhaegal", "Viserion", "Tatsu"],
+            answer: 1
+        }
+    ];
     const result = [
         // Result for more than 70% correct
         {
@@ -78,9 +78,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
             comment: "Dracarys."
         }
-    ]});
-    //Function for next questions
-    function start() {
+    ];
+});
+//Function for next questions
+function start() {
     fadeIn(document.getElementById('quiz-questions'), 200);
 
     if (questionNumber !== allQuestions.length) {
@@ -166,3 +167,22 @@ function answerCheck(userAnswer) {
         document.getElementById(userAnswer).classList.add("incorrectStyle");
     }
 }
+// Event listener for answer buttons
+document.querySelectorAll('.submit-answer').forEach(function (button) {
+    button.addEventListener("click", function (event) {
+        var userAnswer = parseInt(event.target.id);
+        answerCheck(userAnswer);
+
+        setTimeout(function () {
+            document.querySelectorAll(".submit-answer").forEach(function (btn) {
+                btn.classList.remove("correctStyle", "incorrectStyle");
+            });
+            start();
+        }, 1500);
+
+        questionNumber++;
+    });
+});
+
+// Start the quiz
+start();
